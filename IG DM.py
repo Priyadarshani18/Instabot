743cd1e47b0add85887c06f82b97918121a229f8
Priyadarshani18 <143783646+Priyadarshani18@users.noreply.github.com>
2023-10-06 14:23:20 +0530

    Update IG DM.py

--- IG DM.py
+++ IG DM.py
@@ -2,10 +2,10 @@ from instadm.instadm import InstaDM
 
 if __name__ == '__main__':
 	# Auto login
-	insta = InstaDM(username='gymfeaks', password='Password1', headless=False)
+	insta = InstaDM(username='the_reader', password='Password1', headless=False)
 	
 	# Send message
-	insta.sendMessage(user='itsreetik', message='Hey there')
+	insta.sendMessage(user='its me', message='Hey there!')
 	
 	# Send message
 	#insta.sendGroupMessage(users=['user1', 'user2'], message='Hey !')
